e0aa1fac5128f59b1ae96f6256a4a0a6f6ac412c
ftnetops <65729911+ftnetops@users.noreply.github.com>
2026-01-08 13:28:30 -0600

    Add thingProperties.h for IoT configuration
    thing properties for IoT config for Giga R1

---
+++ thingProperties.h
@@ -0,0 +1,27 @@
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <Arduino_NetworkConfigurator.h>
+#include "configuratorAgents/agents/BLEAgent.h"
+#include "configuratorAgents/agents/SerialAgent.h"
+
+const char SSID[]     = SECRET_SSID;    // Network SSID (name)
+const char PASS[]     = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
+
+KVStore kvStore;
+BLEAgentClass BLEAgent;
+SerialAgentClass SerialAgent;
+WiFiConnectionHandler ArduinoIoTPreferredConnection; 
+NetworkConfiguratorClass NetworkConfigurator(ArduinoIoTPreferredConnection);
+
+void initProperties(){
+  NetworkConfigurator.addAgent(BLEAgent);
+  NetworkConfigurator.addAgent(SerialAgent);
+  NetworkConfigurator.setStorage(kvStore);
+  // For changing the default reset pin uncomment and set your preferred pin. Use DISABLE_PIN for disabling the reset procedure.
+  //NetworkConfigurator.setReconfigurePin(your_pin);
+  ArduinoCloud.setConfigurator(NetworkConfigurator);
+
+WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
+
+
+}
